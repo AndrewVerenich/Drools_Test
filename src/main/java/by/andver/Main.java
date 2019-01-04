ec4bcc74eff7d4755fbc64812ecbe6b32dc11a1a
AndrewVerenich <kakxosh@yandex.ru>
2019-01-04 15:23:33 +0300

    init commit

---
+++ src/main/java/by/andver/Main.java
@@ -0,0 +1,11 @@
+package by.andver;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("app-context.xml");
+
+    }
+}
