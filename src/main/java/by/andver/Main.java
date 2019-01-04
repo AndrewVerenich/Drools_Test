907a660fcda357918411f29da63105b2e30a747c
AndrewVerenich <kakxosh@yandex.ru>
2019-01-04 17:02:34 +0300

    spring kie

--- src/main/java/by/andver/Main.java
+++ src/main/java/by/andver/Main.java
@@ -1,11 +1,31 @@
 package by.andver;
 
+import org.kie.api.KieBase;
+import org.kie.api.runtime.KieSession;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         ApplicationContext context=new ClassPathXmlApplicationContext("app-context.xml");
+
+
+        KieBase kieBase= (KieBase) context.getBean("drl_kiesample");
+        KieSession kieSession=kieBase.newKieSession();
+
+        Employee employee=new Employee();
+        employee.setAge(25);
+
+        Employee employee1=new Employee();
+        employee1.setAge(18);
+
+
+
+        kieSession.insert(employee);
+        kieSession.fireAllRules();
+        kieSession.dispose();
 
     }
 }
