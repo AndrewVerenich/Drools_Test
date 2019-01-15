c58e9bf023ac988023bd43356ab061cb86fc4e50
AndrewVerenich <kakxosh@yandex.ru>
2019-01-15 16:21:01 +0300

    download and split multiple rules files

--- buildSrc/src/main/groovy/by/scripts/XlsSplitter.groovy
+++ buildSrc/src/main/groovy/by/scripts/XlsSplitter.groovy
@@ -4,39 +4,55 @@ import groovy.json.JsonSlurper
 import org.apache.poi.hssf.usermodel.HSSFSheet
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 
-
 class XlsSplitter {
-    static void split(String path, String rootDir, String downloadPath, String relResourcePath){
-        def rulesJson=new JsonSlurper().parseText(new File(path).text)
-        rulesJson.files.each{
-            def fileName=it.fileName
-            it.documents.each{
-                splitOnSheets(fileName+'.xls',
+    static void split(String documentDescriptor, String rootDir, String downloadPath, String relResourcePath) {
+        def rulesJson = new JsonSlurper().parseText(new File(documentDescriptor).text)
+        rulesJson.files.each {
+            def docName = it.fileName
+            def projectName = it.projectName
+            it.documents.each {
+                def sheetName = it.getKey()
+                def newSheetName = sheetName
+                if (!it.getValue().sheetName.equals(""))
+                    newSheetName = it.getValue().sheetName
+                if (!it.getValue().projectName.equals(""))
+                    projectName = it.getValue().projectName
+                splitOnSheets(docName + '.xls',
                         downloadPath,
-                        rootDir+'/'+it.projectName+relResourcePath,
-                        it.sheetName)
+                        rootDir + '/' + projectName + '/' + relResourcePath,
+                        sheetName,
+                        newSheetName)
             }
         }
     }
 
-    static void splitOnSheets(String fileName,String downloadPath, String targetPath, String sheetName){
+    static void splitOnSheets(String docName, String downloadPath, String targetPath, String sheetName, String newSheetName) {
+        HSSFWorkbook workbook = getWorkbook(downloadPath, docName)
+        removeAllSheetsExceptOne(workbook, sheetName, newSheetName)
+        saveWorkbook(targetPath, sheetName, workbook)
+    }
 
-        File file=new File(downloadPath+'/'+fileName)
-        FileInputStream inputStream=new FileInputStream(file)
-        HSSFWorkbook workbook=new HSSFWorkbook(inputStream)
+    private static HSSFWorkbook getWorkbook(String downloadPath, String docName) {
+        File file = new File(downloadPath + '/' + docName)
+        FileInputStream inputStream = new FileInputStream(file)
+        HSSFWorkbook workbook = new HSSFWorkbook(inputStream)
+        workbook
+    }
 
-        for(int i=workbook.getNumberOfSheets()-1;i>=0;i--){
-            HSSFSheet tmpSheet =workbook.getSheetAt(i)
-            if(!tmpSheet.getSheetName().equals(sheetName)){
+    private static void saveWorkbook(String targetPath, String sheetName, HSSFWorkbook workbook) {
+        File destFile = new File(targetPath + '/' + sheetName + '.xls')
+        FileOutputStream output = new FileOutputStream(destFile)
+        workbook.write(output)
+    }
+
+    private static void removeAllSheetsExceptOne(HSSFWorkbook workbook, String sheetName, String newSheetName) {
+        for (int i = workbook.getNumberOfSheets() - 1; i >= 0; i--) {
+            HSSFSheet tmpSheet = workbook.getSheetAt(i)
+            if (!tmpSheet.getSheetName().equals(sheetName)) {
                 workbook.removeSheetAt(i)
             }
         }
-
-        inputStream.close()
-
-        File destFile=new File(targetPath+'/'+sheetName+'.xls')
-        FileOutputStream output = new FileOutputStream(destFile)
-        workbook.write(output)
-        output.close()
+        workbook.setSheetName(0, newSheetName)
     }
+
 }
